fbe670b778404c7dff5b249ab74e826d1c2d5313
Mahmoud_Gad <hooda.gad333@gmail.com>
2024-11-11 12:10:14 +0200

    vercel fixxx

--- plugins/vite-node-plugin.ts
+++ plugins/vite-node-plugin.ts
@@ -8,7 +8,7 @@ export const vitePluginNode = () =>
     adapter: "express",
 
     // tell the plugin where is your project entry
-    appPath: "./src/index.ts",
+    appPath: "./src/app.ts",
 
     // Optional, default: 'viteNodeApp'
     // the name of named export of you app from the appPath file
