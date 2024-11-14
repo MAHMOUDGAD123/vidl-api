2dda759289b59f8c82369ba35ae538b79d8400b5
Mahmoud_Gad <hooda.gad333@gmail.com>
2024-11-14 10:23:43 +0200

    fixing the session N

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -4,13 +4,6 @@ import { openDownloadSessionHandler } from "./youtube/ytHandlers";
 
 const router: Router = Router();
 
-router.use((_, response, next) => {
-  response.setHeader("Access-Control-Allow-Origin", "*");
-  response.setHeader("Access-Control-Allow-Methods", "POST, GET");
-  response.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
-
 router.get("/ods", openDownloadSessionHandler);
 
 // routes
