b03e6ff84bb347cf6a8f3bab2624d789d20b2e22
Mahmoud_Gad <hooda.gad333@gmail.com>
2024-11-17 07:29:45 +0200

    fix origin error A

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,8 @@ const app: Express = express();
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: false, limit: "10mb" }));
 app.use(cookieParser());
-// app.use(session(SESSION_CONFIG));
 app.use(cors(CORS_OPTIONS));
-app.use("/api", routes);
+app.use("api", routes);
 
 app.get("/", async (_: Request, response: Response) => {
   response.status(200).json({
