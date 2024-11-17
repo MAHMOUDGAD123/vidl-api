4c8d1222c33df02134da0d07fd64755094482e74
Mahmoud_Gad <hooda.gad333@gmail.com>
2024-11-17 07:31:44 +0200

    fix origin error A

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: false, limit: "10mb" }));
 app.use(cookieParser());
 app.use(cors(CORS_OPTIONS));
-app.use("api", routes);
+app.use("/api", routes);
 
 app.get("/", async (_: Request, response: Response) => {
   response.status(200).json({
