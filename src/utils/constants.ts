472f3855e6258a00c4927d7be9bbfdab40dc4099
Mahmoud_Gad <hooda.gad333@gmail.com>
2025-04-28 00:47:46 +0300

    Update: update cache by adding refreshThreshold

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -432,6 +432,13 @@ export const CORS_OPTIONS: CorsOptions = {
   optionsSuccessStatus: 200,
 };
 
-export const CACHE: { TTL: number } = {
-  TTL: 10 * 60 * 1000, // 10 minutes
+const CACHE_TTL = 10 * 60 * 1000; // 10 minutes
+
+export const CACHE: { TTL: number; refreshThreshold: () => number } = {
+  TTL: CACHE_TTL,
+  refreshThreshold: () => {
+    // to update the cache 10 seconds before outdated
+    const threshold = 10000;
+    return CACHE_TTL - threshold < 0 ? 0 : threshold;
+  },
 };
