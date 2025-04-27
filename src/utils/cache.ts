97cd777a86fa80a2a788bb963cffe950dc0fe632
Mahmoud_Gad <hooda.gad333@gmail.com>
2025-04-28 01:12:59 +0300

    update: yt cookies

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -1,6 +1,17 @@
 import { createCache } from "cache-manager";
-import { CACHE } from "@/utils/constants";
+
+const CACHE_TTL = 10 * 60 * 1000; // 10 minutes
+
+const CACHE: { TTL: number; refreshThreshold: () => number } = {
+  TTL: CACHE_TTL,
+  refreshThreshold: () => {
+    // to update the cache 10 seconds before outdated
+    const threshold = 10000;
+    return CACHE_TTL - threshold < 0 ? 0 : threshold;
+  },
+};
 
 export const memCache = createCache({
   ttl: CACHE.TTL,
+  refreshThreshold: CACHE.refreshThreshold(),
 });
